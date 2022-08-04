26fcaa67052d57f97026bf492019f73e62dc6f25
Valerii Martell <valerii.martell@gmail.com>
2022-08-05 00:58:02 +0200

    Update routing examples

--- routing/urls.py
+++ routing/urls.py
@@ -3,11 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.routing_index),
-    path('redirect/', views.http_redirect),
-    path('file-response/', views.file_response),
+    path('redirect/', views.http_redirect, name='redirect'),
+    path('file-response/', views.file_response, name='file'),
     path('function/', views.function),
     path('function/<int:year>/<int:month>/<int:day>/', views.function),
     path('render-html/', views.render_html),
     path('render-template/', views.render_template),
     path('request-properties/', views.request_properties),
+    path('not-allowed/', views.not_allowed, name='not_allowed'),
+    path('render-to-string/', views.render_to_string, name="render_to_string"),
+    path('text/', views.text, name='text'),
+    path('json/', views.json, name='json'),
 ]
--- routing/views.py
+++ routing/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse, HttpResponseRedirect, \
+    HttpResponseNotAllowed, JsonResponse
+from django.shortcuts import render
+from django.templatetags.static import static
+from django.views import View
+from django.template import loader
 
 
 def routing_index(request):
@@ -14,11 +18,15 @@ def routing_index(request):
             <h1>Routing index:</h1>
             <p><a href="/">001. Back to main index</a></p>
             <p><a href="/routing/redirect">002. Redirect</a></p>
-            <p><a href="/routing/file-response">003. File Response TODO</a></p>
-            <p><a href="/routing/function/1996/08/30">004. Function with parameters /year/month/day or / - default values </a></p>
-            <p><a href="/routing/render-html">005. Render HTML string </a></p>
-            <p><a href="/routing/render-template">006. Render HTML template </a></p>
-            <p><a href="/routing/request-properties">007. Request additional properties </a></p>
+            <p><a href="/routing/text">003. Text Response</a></p>
+            <p><a href="/routing/file-response">004. File Response</a></p>
+            <p><a href="/routing/json">005. JSON Response</a></p>
+            <p><a href="/routing/not-allowed">006. Not allowed</a></p>
+            <p><a href="/routing/function/1996/08/30">007. Function with parameters /year/month/day or / - default values </a></p>
+            <p><a href="/routing/render-html">008. Render HTML string </a></p>
+            <p><a href="/routing/render-template">009. Render HTML template </a></p>
+            <p><a href="/routing/render-to-string">010. Render to string </a></p>
+            <p><a href="/routing/request-properties">011. Request additional properties </a></p>
         </body>
     </html>
     """
@@ -27,12 +35,15 @@ def routing_index(request):
 
 
 def http_redirect(request):
-    return redirect("/")
+    # return redirect("/")
+    return HttpResponseRedirect("http://www.google.com")
 
 
 def file_response(request):
-    return redirect("/")
-    pass
+    # with open() as file:
+    #     work with file
+    # print(static('images/001.jpg')[1:])
+    return FileResponse(open(static('images/001.jpg')[1:], "rb+"))
 
 
 def function(request, year=1111, month=11, day=1):
@@ -57,7 +68,7 @@ def render_html(request):
 
 
 def render_template(request):
-    return render(request, 'routing.html', {})
+    return render(request, 'main.html', {})
 
 
 def request_properties(request):
@@ -79,3 +90,20 @@ def request_properties(request):
     """.format(request.path, request.get_full_path(), request.get_host(), request.is_secure())
 
     return HttpResponse(_html)
+
+
+def render_to_string(request):
+    test_template = loader.render_to_string("main.html")
+    return HttpResponse(test_template)
+
+
+def text(request):
+    return HttpResponse("This is text from backend to user interface")
+
+
+def not_allowed(request):
+    return HttpResponseNotAllowed("You shall not pass!!!")
+
+
+def json(request):
+    return JsonResponse({i: i + i for i in range(1, 20)}, safe=False)
