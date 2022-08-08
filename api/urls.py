fa53fe986a607c5baf5b9ae82882c21ba2721a3c
Valerii Martell <valerii.martell@gmail.com>
2022-08-09 00:16:23 +0200

    Add function-based, class-based, ViewSet and generic API view examples

--- api/urls.py
+++ api/urls.py
@@ -6,13 +6,17 @@ from . import views
 router = routers.SimpleRouter()
 router.register(r'game', views.GameViewSet)
 router.register(r'gamer', views.GamerViewSet)
-
+router.register(r'view-set', views.ViewSetAPIView)
 
 urlpatterns = [
     path('', views.api_index),
     path('', include(router.urls)),
     path('function/', views.view_function, name='function_view'),
     path('class/', views.ClassAPIView.as_view(), name='class_view'),
+    path('create/', views.MyCreateAPIView.as_view(), name='create'),
+    path('retrieve/<int:pk>', views.MyRetrieveAPIView.as_view(), name='retrieve'),
+    path('retrieve-update/<int:pk>', views.MyRetrieveUpdateAPIView.as_view(),
+         name='retrieve_update'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -20,11 +23,16 @@ def api_index(request):
         <body>
             <h1>Django REST Framework index:</h1>
             <p><a href="/">001. Back to main index</a></p>
-            <p><a href="/api/game">002. Games list</a></p>
-            <p><a href="/api/gamer">003. Gamers list</a></p>
-            <hr/>
-            <p><a href="/api/function">004. Function-based API view</a></p>
-            <p><a href="/api/class">005. CLass-based API view</a></p>
+            <p><a href="/api/function">002. Function-based API view</a></p>
+            <p><a href="/api/class">003. CLass-based API view</a></p>
+            <p><a href="/api/game">004. API ViewSet - games (default)</a></p>
+            <p><a href="/api/gamer">005. API ViewSet - gamers (default)</a></p>
+            <p><a href="/api/view-set">006. API ViewSet - list, retrieve all or by id</a></p>
+            <p><a href="/api/create">007. Generic API view - create</a></p>
+            <p><a href="/api/retrieve/1">008. Generic API view - retrieve by id</a></p>
+            <p><a href="/api/retrieve-update/1">009. Generic API view - retrieve and update by id</a></p>
+            
+            
         </body>
     </html>
     """
@@ -58,3 +66,31 @@ class ClassAPIView(APIView):
     def post(self, request):
         # print(request.data)
         return Response({'class': 'some_class_data'})
+
+
+class ViewSetAPIView(viewsets.ViewSet):
+    queryset = GameModel.objects.filter(id__lte=10)
+
+    def list(self, request):
+        serializer = GameModelSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(self.queryset, pk=pk)
+        serializer = GameModelSerializer(user)
+        return Response(serializer.data)
+
+
+class MyCreateAPIView(CreateAPIView):
+    serializer_class = GamerModelSerializer
+
+
+class MyRetrieveAPIView(RetrieveAPIView):
+    permission_classes = (IsAdminUser,)
+    queryset = GamerModel.objects.all()
+    serializer_class = GamerModelSerializer
+
+
+class MyRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = GamerModel.objects.all()
+    serializer_class = GamerModelSerializer
