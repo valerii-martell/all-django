4b4ffd2988cdcc267f3bbf4d873b11d9aed28929
Valerii Martell <valerii.martell@gmail.com>
2022-08-16 05:36:13 +0200

    Add some templates and views for Celery

--- index/views.py
+++ index/views.py
@@ -27,6 +27,7 @@ def index(request):
             <p><a href="/frontend">014. Frontend</a></p>
             <p><a href="/emails">015. Emails</a></p>
             <p><a href="/graphql">016. GraphQL</a></p>
+            <p><a href="/celery">017. Celery (Redis/RabbitMQ)</a></p>
         </body>
     </html>
     """
