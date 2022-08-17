85f5157e5c57a840c13d1b684c8a883b39743400
Valerii Martell <valerii.martell@gmail.com>
2022-08-17 04:32:07 +0200

    Add rooms' like/dislike system

--- channels_app/consumers.py
+++ channels_app/consumers.py
@@ -64,3 +64,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def get_historical_data(self):
         return Message.objects.filter(room=Room.objects.get(name=self.room_name))
+
+
+class LikesConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        await self.accept()
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+
+        like = text_data_json.get('like', None)
+        dislike = text_data_json.get('dislike', None)
+        if like:
+            await self.new_like()
+        if dislike:
+            await self.new_dislike()
+
+    @database_sync_to_async
+    def new_like(self):
+        room_instance = Room.objects.get(name=self.room_name)
+        room_instance.likes += 1
+        room_instance.save()
+
+    @database_sync_to_async
+    def new_dislike(self):
+        room_instance = Room.objects.get(name=self.room_name)
+        room_instance.dislikes += 1
+        room_instance.save()
--- channels_app/routing.py
+++ channels_app/routing.py
@@ -4,4 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     path('ws/chat/<room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/likes/<room_name>/', consumers.LikesConsumer.as_asgi())
 ]
